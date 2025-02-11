f62b299c17520cc7c83f5f418d58a7357e7267a2
Sebastien <sewelseb@hotmail.com>
2025-02-11 16:24:10 +0100

    refactor: extract Game instance in test file

--- test.js
+++ test.js
@@ -3,17 +3,15 @@ const mocha = require('mocha');
 const Game = require('./Game');
 
 describe("testing the count function", () => {
-    it("should return 1 when given 1",() => {
-        let game = new Game();
+    let game = new Game();
 
+    it("should return 1 when given 1",() => {
         let result = game.count(1);
 
         assert.equal(result, 1);
     });
 
     it("should return 2 when given 2", () => {
-        let game =new Game();
-
         let result = game.count(2);
 
         assert.equal(result, 2);
