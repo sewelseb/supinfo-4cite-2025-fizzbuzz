7f61aa3add55ef9916aa40db93d7e0a2fde35016
Sebastien <sewelseb@hotmail.com>
2025-02-26 09:45:08 +0100

    fix: 15 bug

--- Game.js
+++ Game.js
@@ -1,9 +1,14 @@
 module.exports =  class Game {
     count(number) {
+        if (this.isMultipleOf3and5(number)) return "fizzbuzz";
         if (this.isMultipleOf3(number)) return "fizz";
         if (this.isMultipleOf5(number)) return "buzz";
           
         return number;
+    }
+
+    isMultipleOf3and5(number) {
+        return this.isMultipleOf3(number) && this.isMultipleOf5(number);
     }
 
     isMultipleOf5(number) {
--- test.js
+++ test.js
@@ -3,8 +3,10 @@ const mocha = require('mocha');
 const Game = require('./Game');
 
 describe("testing the count function", () => {
-    let game = new Game();
-
+    beforeEach(() => {
+        let game = new Game();
+    });
+    
     it("should return 1 when given 1",() => {
         let result = game.count(1);
 
@@ -52,4 +54,17 @@ describe("testing the count function", () => {
 
         assert.equal(result, "fizzbuzz");
     });
+
+    it('should throw an exception', () => {
+        var result = "";
+
+        try {
+            const res = math.add('hello',3)
+        } catch (error) {
+            result = error.message;
+        }
+
+        assert.equal(result, 'should be a number');
+    })
+       
 });
